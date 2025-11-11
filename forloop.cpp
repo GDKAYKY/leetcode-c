21f9dd082d31e8597d3ebf8c0d1f0e540ee5e60a
GDKAYKY <gdkayky@proton.me>
2025-11-11 02:22:12 -0300

    initializing repo

---
+++ forloop.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string message = "this is the message";
+    std::cout << message << "\n";
+
+    message = "this message has changed";
+    std::cout << message << "\n";
+
+
+
+    for (int i = 0; i <= 10; i++){
+    std::cout << i << "\n";
+    }
+    std::cout << "for Loop ended\n";
+
+
+    return 0;
+}
