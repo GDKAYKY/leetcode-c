684d7d4f84662408f87db04c100f434961f1b2e5
GDKAYKY <gdkayky@proton.me>
2026-01-13 23:15:29 -0300

    r2r

--- leetcode.cs
+++ leetcode.cs
@@ -1,4 +1,4 @@
-p       ublic class Solution {
+public class Solution {
     public ListNode AddTwoNumbers(ListNode l1, ListNode l2) {
         var firstNode = l1;
         Console.WriteLine (firstNode);
---
+++ roman2int.cpp
@@ -0,0 +1,31 @@
+#include <unordered_map>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    int romanToInt(std::string s) {
+        std::unordered_map<char, int> map = {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+
+        int result = map[s[s.length() - 1]];
+
+        for (int i = s.length() - 2; i >= 0; i--) {
+            if (map[s[i]] >= map[s[i + 1]]) {
+                result += map[s[i]];
+            } else {
+                result -= map[s[i]];
+            }
+        }
+
+        return result;
+    }
+};
